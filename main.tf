67f2c5b17d589964b4dc55fb6a89e2d021c862c5
Oleksandr Krasnoporov <alexbit@outlook.com>
2021-08-22 15:07:46 +0300

    init conf for 2 comps

--- main.tf
+++ main.tf
@@ -3,9 +3,16 @@
 //API Compute Engine API
 //API DNS api
 //export GOOGLE_APPLICATION_CREDENTIALS={C:\Service\cloudx\cloudx-finaltask-3a66417d9198.json}
+//terraform init
+//terraform plan
 
 provider "google" {
-    version = "<=0.11.0"
+    // Compatible with TF11
+    //version = "<=0.11.0"
+    
+    // Compatibel with TF12.31
+    version = "~> 2.5"
+    
     //credentials = file("C:\Service\cloudx\cloudx-finaltask-3a66417d9198.json")
     project = "cloudx-finaltask"
     region = "us-central1"
@@ -21,7 +28,9 @@ resource "google_compute_subnetwork" "vpc_subnet" {
     ip_cidr_range = "10.1.0.0/24"
     name = "us-central1-subnet"
     region = "us-central1"
-    network = "${google_compute_network.vpc_network.name}"
+    // Compatible with TF11
+    //network = "${google_compute_network.vpc_network.name}"
+    network = google_compute_network.vpc_network.name
     private_ip_google_access = true
 
     secondary_ip_range {
@@ -41,8 +50,11 @@ resource "google_compute_subnetwork" "vpc_subnet" {
 
 resource "google_compute_router" "router" {
   name    = "inet-router"
-  region  = "${google_compute_subnetwork.vpc_subnet.region}"
-  network = "${google_compute_network.vpc_network.name}"
+  // Compatible with TF11
+  //region  = "${google_compute_subnetwork.vpc_subnet.region}"
+  region  = google_compute_subnetwork.vpc_subnet.region
+  //network = "${google_compute_network.vpc_network.name}"
+  network = google_compute_network.vpc_network.name
 
     bgp {
     asn = 64514
@@ -52,8 +64,11 @@ resource "google_compute_router" "router" {
 
 resource "google_compute_router_nat" "nat" {
   name                               = "nat-gateway"
-  router                             = "${google_compute_router.router.name}"
-  region                             = "${google_compute_router.router.region}"
+  // Compatible with TF11
+  //router                             = "${google_compute_router.router.name}"
+  //region                             = "${google_compute_router.router.region}"
+  router                             = google_compute_router.router.name
+  region                             = google_compute_router.router.region
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 
