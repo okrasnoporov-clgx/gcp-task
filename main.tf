d9649296914781a3bc734df664a73fd285acd27b
Oleksandr Krasnoporov <alexbit@outlook.com>
2021-08-24 20:24:29 +0300

    Create GKE cluster

--- main.tf
+++ main.tf
@@ -1,10 +1,22 @@
 //API should be enabled on project:
+//-------- gcloud services list --available ---------------
+
+//Identity and Access Management (IAM) API 
+//(gcloud services enable iamcredentials.googleapis.com, gcloud services enable iam.googleapis.com)
 //API [cloudresourcemanager.googleapis.com]
 //API Compute Engine API
 //API DNS api
+//API GKE containers (gcloud services enable container.googleapis.com)
+
+
 //export GOOGLE_APPLICATION_CREDENTIALS={C:\Service\cloudx\cloudx-finaltask-3a66417d9198.json}
 //terraform init
 //terraform plan
+//terraform apply
+
+
+
+//----------------------- ### GOOGLE PROVIDER ### ----------------------------------------//
 
 provider "google" {
     // Compatible with TF11
@@ -17,6 +29,11 @@ provider "google" {
     project = "cloudx-finaltask"
     region = "us-central1"
 }
+
+
+
+//------------------------------ ### NETWORKS ### -----------------------------------------//
+
 
 resource "google_compute_network" "vpc_network" {
   project                 = "cloudx-finaltask"
@@ -33,7 +50,7 @@ resource "google_compute_subnetwork" "vpc_subnet" {
     network = google_compute_network.vpc_network.name
     private_ip_google_access = true
 
-    secondary_ip_range {
+  secondary_ip_range {
         range_name    = "pods"
         ip_cidr_range = "10.2.0.0/20"
   }
@@ -75,5 +92,96 @@ resource "google_compute_router_nat" "nat" {
   log_config {
     enable = true
     filter = "ERRORS_ONLY"
+  }
+}
+
+
+
+//------------------------------ ### SERVICE ACCOUNTS ### --------------------------------------//
+
+resource "google_service_account" "service_account" {
+  account_id   = "nextcloud"
+  display_name = "nextcloud"
+  description = "Next Cloud service account"
+}
+
+resource "google_service_account" "gke_service_account" {
+  account_id   = "kubernetes"
+  display_name = "kubernetes"
+  description = "GKE service account"
+}
+
+
+
+//--------------------------------- ### GKE CLUSTER ### -----------------------------------------//
+
+resource "google_container_cluster" "primary" {
+  name     = "cluster"
+  description = "GKE cluster"
+  location = "us-central1"
+  node_locations = ["us-central1-a", "us-central1-b"]
+  network = google_compute_network.vpc_network.name
+  subnetwork = google_compute_subnetwork.vpc_subnet.name
+  
+  private_cluster_config {
+    enable_private_endpoint = false
+    enable_private_nodes    = true
+    
+    master_ipv4_cidr_block = "172.16.0.0/28"
+  }
+
+  master_authorized_networks_config {
+      cidr_blocks {
+          cidr_block   = "0.0.0.0/0"
+          display_name = "auth-allow-all"
+      }
+  }
+
+  ip_allocation_policy {
+      cluster_secondary_range_name  = google_compute_subnetwork.vpc_subnet.secondary_ip_range[0].range_name
+      services_secondary_range_name = google_compute_subnetwork.vpc_subnet.secondary_ip_range[1].range_name
+  }
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "primary_private_nodes" {
+  name       = "privat-node-pool"
+  location   = "us-central1"
+  cluster    = google_container_cluster.primary.name
+  node_count = 2
+
+  node_config {
+    preemptible  = false
+    machine_type = "n1-standard-1"
+
+    service_account = google_service_account.gke_service_account.email
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
+
+
+
+//---------------------------- ### CLOUD SQL DATABSE MYSQL ### ------------------------------------//
+
+resource "google_sql_database_instance" "mysql_instance" {
+  //provider = google-beta
+
+  name   = "database"
+  database_version = "MYSQL_8_0"
+  region = "us-central1"
+  
+
+  depends_on = [google_service_networking_connection.private_vpc_connection]
+
+  settings {
+    tier = "db-n1-standard-1"
+    //ip_configuration {
+      //ipv4_enabled    = false
+      //private_network = google_compute_network.private_network.id
+    }
   }
 }
